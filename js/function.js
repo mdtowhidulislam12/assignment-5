523bbf83b0593ff57b851aca041300b5ac7e9fbb
MD TOWHIDUL ISLAM <mohammadtowhidulislam999@gmail.com>
2024-09-24 19:40:30 +0600

    second commit

--- js/function.js
+++ js/function.js
@@ -8,15 +8,27 @@ document.querySelector('#Donate-now-btn1').addEventListener('click', function ()
     const donateAmountTk = Number(donateAmount);
     const donateFund = document.querySelector('#donate-balance1').innerText;
     const donateFundTk = Number(donateFund);
+
+    if (isNaN(donateAmount) || donateAmount < 0 || donateAmount === '') {
+        alert('Invalid balance.please try again')
+        
+        return
+
+    }else{
     const donateFundBalance = donateFundTk + donateAmountTk;
 
     document.getElementById('donate-balance1').innerText = donateFundBalance;
 
-    if (isNaN(donateAmount) || donateAmount < 0) {
-        alert('Invalid balance.please try again')
-    }
+    onclickBtn1();
+
+    addTransaction1();
+
+    
+
+
     document.querySelector('#Amount-1').value = '';
 
+    }
 });
 
 //feni donation club  
@@ -28,42 +40,61 @@ document.querySelector('#Donate-now-btn2').addEventListener('click', function ()
     const donateAmountTk = Number(donateAmount);
     const donateFund = document.querySelector('#donate-balance2').innerText;
     const donateFundTk = Number(donateFund);
-    const donateFundBalance = donateFundTk + donateAmountTk;
 
-    document.getElementById('donate-balance2').innerText = donateFundBalance;
-
-    if (isNaN(donateAmount) || donateAmount < 0) {
+    if (isNaN(donateAmount) || donateAmount < 0 || donateAmount === '') {
         alert('Invalid balance.please try again')
-    }
-    document.querySelector('#Amount-2').value = '';
 
+        return
+
+    } else {
+        const donateFundBalance = donateFundTk + donateAmountTk;
+
+        document.getElementById('donate-balance2').innerText = donateFundBalance;
+
+        onclickBtn2();
+        addTransaction2();
+
+
+        document.querySelector('#Amount-2').value = '';
+    }
 });
 
 
 // donate in quta 
 
-document.querySelector('#Donate-now-btn3').addEventListener('click', function () {
+document.querySelector('#Donate-now-btn3').addEventListener('click', function (event) {
 
 
     const donateAmount = document.querySelector('#Amount-3').value;
     const donateAmountTk = Number(donateAmount);
     const donateFund = document.querySelector('#donate-balance3').innerText;
     const donateFundTk = Number(donateFund);
-    const donateFundBalance = donateFundTk + donateAmountTk;
 
-    document.getElementById('donate-balance3').innerText = donateFundBalance;
-
-    if (isNaN(donateAmount) || donateAmount < 0) {
+    if (isNaN(donateAmount) || donateAmount < 0 || donateAmount === '') {
         alert('Invalid balance.please try again')
+
+        return
+
+    } else {
+        const donateFundBalance = donateFundTk + donateAmountTk;
+
+        document.getElementById('donate-balance3').innerText = donateFundBalance;
+
+        onclickBtn3();
+        addTransaction3();
+
+
+        document.querySelector('#Amount-3').value = '';
+
     }
-    document.querySelector('#Amount-3').value = '';
+
 
 });
 
 // function for onclick
 //btn one
 
-function onclickBtn1(){
+function onclickBtn1() {
 
     const inputAmount = document.getElementById('Amount-1').value;
     const inputTk = Number(inputAmount)
@@ -75,12 +106,12 @@ function onclickBtn1(){
 
     document.getElementById('main-balance').innerText = currentBalance;
 
-    console.log('clicked' , currentBalance)
+    console.log('clicked', currentBalance)
 };
 
 //btn two
 
-function onclickBtn2(){
+function onclickBtn2() {
 
     const inputAmount = document.getElementById('Amount-2').value;
     const inputTk = Number(inputAmount)
@@ -92,12 +123,12 @@ function onclickBtn2(){
 
     document.getElementById('main-balance').innerText = currentBalance;
 
-    console.log('clicked' , currentBalance)
+    console.log('clicked', currentBalance)
 };
 
 //btn three
 
-function onclickBtn3(){
+function onclickBtn3() {
 
     const inputAmount = document.getElementById('Amount-3').value;
     const inputTk = Number(inputAmount)
@@ -109,6 +140,40 @@ function onclickBtn3(){
 
     document.getElementById('main-balance').innerText = currentBalance;
 
-    console.log('clicked' , currentBalance)
+    console.log('clicked', currentBalance)
 };
+
+//navigation 
+// donation page
+document.getElementById('donation-btn').addEventListener('click', function () {
+
+    document.querySelector('#History-container').classList.add('hidden');
+    document.querySelector('#donation-page').classList.add('hidden');
+
+    document.querySelector('#donation-btn').classList.add('bg-[#B4F461]')
+
+    document.querySelector('#History-btn').classList.remove('bg-[#B4F461]')
+
+    document.getElementById('donation-page').classList.remove('hidden')
+
+    console.log('clicked')
+});
+
+//history page
+
+document.querySelector('#History-btn').addEventListener('click', function () {
+    document.querySelector('#History-container').classList.add('hidden');
+    document.querySelector('#donation-page').classList.add('hidden');
+
+    document.querySelector('#History-btn').classList.add('bg-[#B4F461]')
+
+    document.querySelector('#donation-btn').classList.remove('bg-[#B4F461]')
+
+
+    document.getElementById('History-container').classList.remove('hidden')
+
+    console.log('clicked')
+})
+
+
 
--- js/utility.js
+++ js/utility.js
@@ -1,18 +1,96 @@
 
 
-// // Decrease money  from top balance 
-// function topBalance(){
-//     const inputValue = document.querySelector('#Amount-1').value;
-//     const amount = Number(inputValue);
+function showSectionById(id) {
+    document.querySelector('#History-container').classList.add('hidden');
+    document.querySelector('#donation-page').classList.add('hidden');
 
-//     const mainBalance = document.querySelector('#main-balance').innerText;
-//     const mainAmountTk = Number(mainBalance);
-    
-//     const currentBalance = mainAmountTk + amount;
+    document.getElementById(id).classList.remove('hidden')
+}
 
-//     document.getElementById('donate-balance1').innerText = currentBalance;
 
-    
+//create element for history
 
-//     console.log(currentBalance);
-// }
+//donation-1
+function addTransaction1() {
+    const now = new Date();
+    const date = now.toLocaleDateString();
+    const options = { hour: '2-digit', minute: '2-digit', hour12: true };
+    const currentTime = now.toLocaleTimeString([], options);
+
+    const amount = document.getElementById('Amount-1').value;
+
+    const transactionContainer = document.getElementById('History-container');
+    const newTransaction = document.createElement('div');
+    newTransaction.style.border = '1px solid gray-500';
+    newTransaction.style.borderRadius = '1rem';
+    newTransaction.style.padding = '10px';
+
+    const transactionContent = `
+    <div class = " border rounded-lg p-4">
+       <p class = "text-xl font-bold"> ${amount} Taka is donated for famine-2024 at Noyakhali,Bangladesh. </P> <br>
+       <p> Date: ${date}     Time: ${currentTime} (Bangladesh standard time) </p>
+       </div>
+    `;
+    newTransaction.innerHTML = transactionContent;
+
+    transactionContainer.appendChild(newTransaction);
+
+};
+
+//donation-2
+function addTransaction2() {
+    const now = new Date();
+    const date = now.toLocaleDateString();
+
+    const options = { hour: '2-digit', minute: '2-digit', hour12: true };
+    const currentTime = now.toLocaleTimeString([], options);
+
+    const amount = document.getElementById('Amount-2').value;
+
+    const transactionContainer = document.getElementById('History-container');
+    const newTransaction = document.createElement('div');
+    newTransaction.style.border = '1px solid gray-500';
+    newTransaction.style.borderRadius = '1rem';
+    newTransaction.style.padding = '10px';
+
+    const transactionContent = `
+    <div class = " border rounded-lg p-4">
+       <p class = "text-xl font-bold"> ${amount} Taka is donated for flood relief in Feni,Bangladesh. </P> <br>
+       <p> Date: ${date}     Time: ${currentTime} (Bangladesh standard time) </p>
+       </div>
+    `;
+    newTransaction.innerHTML = transactionContent;
+
+    transactionContainer.appendChild(newTransaction);
+
+};
+
+//donation-3
+
+function addTransaction3() {
+    const now = new Date();
+    const date = now.toLocaleDateString();
+
+    const options = { hour: '2-digit', minute: '2-digit', hour12: true };
+    const currentTime = now.toLocaleTimeString([], options);
+
+    const amount = document.getElementById('Amount-3').value;
+
+    const transactionContainer = document.getElementById('History-container');
+    const newTransaction = document.createElement('div');
+    newTransaction.style.border = '1px solid gray-500';
+    newTransaction.style.borderRadius = '1rem';
+    newTransaction.style.padding = '10px';
+
+    const transactionContent = `
+    <div class = " border rounded-lg p-4">
+       <p class = "text-xl font-bold"> ${amount} Taka is donated for Aid fir injured in the Quota movement,Bangladesh. </P> <br>
+       <p> Date: ${date}     Time: ${currentTime} (Bangladesh standard time) </p>
+       </div>
+    `;
+    newTransaction.innerHTML = transactionContent;
+
+    transactionContainer.appendChild(newTransaction);
+
+};
+
